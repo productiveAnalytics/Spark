27e945b709b46215fad6883f8dce928f6483d2f6
Productive Analytics <prod8ctive.analytics@gmail.com>
2018-09-20 13:28:10 -0500

    improved ratings-counter by removing header from CSV

--- ratings-counter.py
+++ ratings-counter.py
@@ -7,14 +7,20 @@ conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
 user_home = Path.home()
-# Absolute path : /home/lalitstar/Dev/Python/Spark/Data/ml-latest/ratings.csv
-# OR file_path = user_home / 'Dev/Python/Spark/Data/ml-latest/ratings.csv'
-datafile_path = user_home.joinpath('Dev/Python/Spark/Data/ml-latest/ratings.csv')
+# Absolute path : /home/lalitstar/Dev/Data/ml-latest/ratings.csv
+# OR file_path = user_home / 'Dev/Data/ml-latest/ratings.csv'
+datafile_path = user_home.joinpath('Dev/Data/ml-latest/ratings.csv')
 file_name = str(datafile_path)
 
 print ('File: ', file_name , ' exists? ', os.path.exists(file_name), ' isFile? ', os.path.isfile(file_name))
 
 lines = sc.textFile(file_name)
+
+# Remove header from CSV
+header_row = lines.first()
+print("Header:", header_row)
+lines = lines.filter(lambda l: l != header_row)
+
 ratings = lines.map(lambda x: x.split(',')[2])
 result = ratings.countByValue()
 
