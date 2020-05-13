f2eaed3a736fc7d12e665a9ce69c5d33e0fb244f
Lalit D. Chawathe <Lalit.X.Chawathe.-nd@disney.com>
2020-05-13 17:25:51 -0500

    Read Parquet file using multiple ways

--- read_parquet_schema.py
+++ read_parquet_schema.py
@@ -7,7 +7,38 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as paq
-t = paq.read_table('test-data/flight_2020-04-27_part_0.parquet')
-df = t.to_pandas()
-print(pa.Schema.from_pandas(df))
-print(df)
+
+PARQUET_FIlE_PATH = 'test-data/flight_2020-04-27_part_0.parquet'
+
+table = paq.read_table(source=PARQUET_FIlE_PATH)
+pandas_df = table.to_pandas()
+
+schema = pa.Schema.from_pandas(pandas_df)
+print('====== Schema (thru PyArrow/Pandas) ======')
+print(schema)
+print()
+print('====== Data ======')
+print(pandas_df)
+
+# ---------
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("PySpark_Parquet_schema") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+sc.setLogLevel('ERROR')
+
+spark_df = spark.read.parquet(PARQUET_FIlE_PATH)
+
+print('====== Schema (Spark) ======')
+spark_df.printSchema()
+print()
+print('====== Data ======')
+spark_df.show(n=10, truncate=False)
+
+spark.stop()
